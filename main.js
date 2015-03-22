1cb22cc0d70dfe4e837be24a655e601f50afd8e6
kenrick95 <kenrick95@gmail.com>
2015-03-22 20:24:07 +0800

    +parse time for view

--- main.js
+++ main.js
@@ -9,12 +9,35 @@ document.addEventListener("DOMContentLoaded", function () {
     canvas.width = window.innerWidth;
     ctx.font = '10em "Fira Sans", sans-serif';
     ctx.fillStyle = "#111";
+    function pad(number, digits) {
+        if (digits === undefined || digits < 2) {
+            digits = 2;
+        }
+        if (number < Math.pow(10, digits - 1)) {
+            var ret = "", i;
+            for (i = 0; i < digits - 1; i += 1) {
+                ret = "0" + ret;
+            }
+            return ret + number;
+        }
+        return number;
+    }
+    function parse(time) {
+        var ms = time % 1000, s, mins, hrs;
+        time = parseInt(time / 1000, 10);
+        s = time % 60;
+        time = parseInt(time / 60, 10);
+        mins = time % 60;
+        time = parseInt(time / 60, 10);
+        hrs = time;
+        return pad(hrs, 3) + ":" + pad(mins) + ":" + pad(s) + "." + pad(ms, 3);
+    }
     function show(timer) {
         // Profiling performance: this is the bottleneck, updating DOM is really costly
         // element.textContent = timer.current;
         // updating SVG is fast enough, although some performance drop occurs
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText(timer.current, 0, canvas.height);
+        ctx.fillText(parse(timer.current), 0, canvas.height);
     }
     function tick(timer) {
         // console.log(timer);
